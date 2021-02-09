75ce3c38b45feb3040ebee66f285ff2a9ae2dbde
Zach Lowden <zach@mobl.ca>
2021-02-09 00:15:17 +0000

    run gazelle

--- helper/access_control.go
+++ helper/access_control.go
@@ -3,20 +3,20 @@ package helper
 import (
 	"context"
 
+	"cloud.google.com/go/firestore"
 	"github.com/casbin/casbin/v2"
 	firestoreadapter "github.com/reedom/casbin-firestore-adapter"
-	"gitlab.innovationup.stream/innovation-upstream/iu-common-go/repo"
+	access_control "gitlab.innovationup.stream/innovation-upstream/iu-common-go/access-control"
 	"unknwon.dev/clog/v2"
 )
 
-func GetAccessControl(ctx context.Context) AccessControl {
-	db := repo.NewFirestoreClient(ctx)
+func GetAccessControl(ctx context.Context, db *firestore.Client) access_control.AccessControl {
 	a := firestoreadapter.NewAdapter(db)
 	enforcer, err := casbin.NewEnforcer("rbac_model.conf", a)
 	if err != nil {
 		clog.Fatal("%+v", err)
 	}
 
-	ac := NewAccessControl(enforcer)
+	ac := access_control.NewAccessControl(enforcer)
 	return ac
 }
