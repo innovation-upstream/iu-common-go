af6e6796146f3c00d288b3a8d6ee235397577bfa
Zach Lowden <zach@mobl.ca>
2021-02-10 05:52:12 +0000

    only auth gcp metadata if in production environment

--- relay/grpc_auth_helper.go
+++ relay/grpc_auth_helper.go
@@ -28,29 +28,33 @@ func NewGRPCAuthHelper() GRPCAuthHelper {
 }
 
 func (g *grpcAuthHelper) AuthorizeGCPContext(ctx context.Context, addr string) (context.Context, error) {
-	splitAddr := strings.Split(addr, ":")
-	tokenSource, err := idtoken.NewTokenSource(ctx, fmt.Sprintf("https://%v", splitAddr[0]))
-	if err != nil {
-		return nil, fmt.Errorf("idtoken.NewTokenSource: %v", err)
-	}
-	token, err := tokenSource.Token()
-	if err != nil {
-		return nil, fmt.Errorf("TokenSource.Token: %v", err)
+	if os.Getenv("ENVIRONMENT") == "production" {
+		splitAddr := strings.Split(addr, ":")
+		tokenSource, err := idtoken.NewTokenSource(ctx, fmt.Sprintf("https://%v", splitAddr[0]))
+		if err != nil {
+			return nil, fmt.Errorf("idtoken.NewTokenSource: %v", err)
+		}
+		token, err := tokenSource.Token()
+		if err != nil {
+			return nil, fmt.Errorf("TokenSource.Token: %v", err)
+		}
+
+		ctx = grpcMetadata.NewOutgoingContext(ctx, grpcMetadata.MD{})
+		ctx = grpcMetadata.AppendToOutgoingContext(ctx, "authorization", "Bearer "+token.AccessToken)
+		return ctx, nil
 	}
 
-	ctx = grpcMetadata.NewOutgoingContext(ctx, grpcMetadata.MD{})
-	ctx = grpcMetadata.AppendToOutgoingContext(ctx, "authorization", "Bearer "+token.AccessToken)
 	return ctx, nil
 }
 
 func (g *grpcAuthHelper) DialRPCService(ctx context.Context, rpcEndpoint string, tracer trace.Tracer) (context.Context, *grpc.ClientConn, error) {
+	ctx, err := g.AuthorizeGCPContext(ctx, rpcEndpoint)
+	if err != nil {
+		return ctx, nil, err
+	}
+
 	var opts []grpc.DialOption
 	if os.Getenv("ENVIRONMENT") == "production" {
-		ctx, err := g.AuthorizeGCPContext(ctx, rpcEndpoint)
-		if err != nil {
-			return ctx, nil, err
-		}
-
 		creds := credentials.NewTLS(&tls.Config{
 			InsecureSkipVerify: true,
 		})
