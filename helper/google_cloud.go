494e2ba17fc6015733d07ae135b270013203fd16
Zach Lowden <zach@mobl.ca>
2021-02-25 21:05:47 +0000

    respect GCLOUD_PROJECT_ID

--- helper/google_cloud.go
+++ helper/google_cloud.go
@@ -5,6 +5,10 @@ import "os"
 func GetProjectID() string {
 	projectID := os.Getenv("GCP_PROJECT_ID")
 	if projectID == "" {
+		projectID = os.Getenv("GCLOUD_PROJECT_ID")
+	}
+
+	if projectID == "" {
 		// Magic value to attempt to auto detect the project id using application default credentials
 		projectID = "*detect-project-id*"
 	}
